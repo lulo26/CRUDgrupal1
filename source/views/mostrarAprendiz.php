9e7e08814145374fb6566daf762e3f78c3486477
DiegoValdesR <bitardos8@gmail.com>
2024-09-25 12:14:01 -0500

    Corrección en las rutas del css y creacion de la modal para editar los estudaintes registrados

--- source/views/mostrarAprendiz.php
+++ source/views/mostrarAprendiz.php
@@ -13,7 +13,7 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     
-    <link rel="stylesheet" href="/assets/tabla-estudiantes.css">
+    <link rel="stylesheet" href="/talleresphp/CRUDgrupal1/assets/tabla-estudiantes.css">
 
 </head>
 <body>
@@ -22,6 +22,7 @@
 
     <div class="row">
       <div class="col-12 btn-volver">
+        
         <a href="/talleresphp/CRUDgrupal1/source/views/crearAprendiz.php">Volver</a>
       </div>
     </div>
@@ -60,7 +61,9 @@
               <td> <?php echo htmlspecialchars($user['telefono']) ?></td>
 
               <td> <?php echo htmlspecialchars($user['correo']) ?></td>
-              <td>  <i class="bi bi-pencil-square"></i></td>
+
+              <td>  <i data-bs-toggle="modal"
+              data-bs-target="#modal-editar-estudiante" class="bi bi-pencil-square"></i></td>
               <?php endforeach; ?>
             </tr>
             <?php else: ?>
@@ -74,10 +77,76 @@
       </div>
     </div>
 
+    <button data-bs-toggle="modal" data-bs-target="#modal-editar-estudiante">Abrir</button>
+    <!--MODAL PARA EDITAR ESTUDIANTES-->
+    <div class="row">
+        <div class="col-12">
+            <div class="modal fade" tabindex="-1" id="modal-editar-estudiante">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title">Editar estudiante</h5>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                    <form class="formulario">
+                      <div class="mb-3">
+                        <span>Numero de documento</span>
+                        <input type="number" class="form-control">
+                      </div>
+
+                      <div class="mb-3">
+                        <span>Nombre completo</span>
+                        <input type="text" class="form-control" >
+                      </div>
+
+                      <div class="mb-3">
+                        <span>Apellido(s)</span>
+                        <input type="text" class="form-control" >
+                      </div>
+        
+                      <div class="mb-3">
+                        <span>Curso</span>
+                        <select class="form-select" aria-label="Default select example">
+                          <option value="A">A</option>
+                          <option value="B">B</option>
+                          <option value="C">C</option>
+                        </select>
+                      </div>
+
+                      <div class="mb-3">
+                        <span>Correo electrónico</span>
+                        <input type="email" class="form-control">
+                      </div>
+
+                      <div class="mb-3">
+                        <span>Número de teléfono</span>
+                        <input type="text" class="form-control">
+                      </div>
+
+                    </form>
+
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar</button>
+                      <button type="button" class="btn btn-primary">Guardar Cambios</button>
+                    </div>
+                  </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
   </div>
+  <script>
+    const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
 
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
+  </script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
 
 </body>
 
